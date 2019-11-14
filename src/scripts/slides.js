cab7b0bc5cf368df16007dd8c4b6b2a6e9741764
Ilya Lakhin <eliah.lakhin@gmail.com>
2019-11-14 07:15:41 +0700

    Brief

--- src/scripts/slides.js
+++ src/scripts/slides.js
@@ -1,6 +1,6 @@
 import 'reveal.js/css/reset.css';
 import 'reveal.js/css/reveal.css';
-import 'reveal.js/css/theme/white.css';
+import 'reveal.js/css/theme/night.css';
 import Reveal from 'reveal.js';
 
 Reveal.initialize();
